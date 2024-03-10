634a697b8b30ac765b193df4cffa97a4c3eefde4
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2024-03-11 01:04:32 +0800

    Fixed a bug where AI-controlled pikemen would continuously switch between shield wall and circle formation when facing cavalry.

--- BetterPikesBehaviorCharge.cs
+++ BetterPikesBehaviorCharge.cs
@@ -7,20 +7,16 @@ namespace BetterPikes
         protected static void Postfix(BehaviorCharge __instance, ref MovementOrder ____currentOrder)
         {
             Formation formation = __instance.Formation;
+            FormationQuerySystem closestEnemyFormation = formation.QuerySystem.ClosestEnemyFormation;
 
-            if (formation.CountOfUnits > 1 && formation.GetCountOfUnitsWithCondition(agent => agent.WieldedWeapon.CurrentUsageItem?.ItemUsage == "polearm_pike") >= formation.CountOfUnits * BetterPikesSettings.Instance.MinPikemenPercentInPikeFormation)
+            if (formation.CountOfUnits > 1 && formation.GetCountOfUnitsWithCondition(agent => agent.WieldedWeapon.CurrentUsageItem?.ItemUsage == "polearm_pike") >= formation.CountOfUnits * BetterPikesSettings.Instance.MinPikemenPercentInPikeFormation && closestEnemyFormation != null && !closestEnemyFormation.IsCavalryFormation && !closestEnemyFormation.IsRangedCavalryFormation)
             {
-                FormationQuerySystem closestEnemyFormation = formation.QuerySystem.ClosestEnemyFormation;
-
                 // If the percentage of pikemen is above a certain limit, make the formation into a deep shield wall.
                 formation.ArrangementOrder = ArrangementOrder.ArrangementOrderShieldWall;
                 formation.FormOrder = FormOrder.FormOrderDeep;
 
-                if (closestEnemyFormation != null && !closestEnemyFormation.IsCavalryFormation && !closestEnemyFormation.IsRangedCavalryFormation)
-                {
-                    // If the pikemen's closest enemy formation is not cavalry, make the pikemen advance.
-                    ____currentOrder = MovementOrder.MovementOrderAdvance;
-                }
+                // If the pikemen's closest enemy formation is not cavalry, make the pikemen advance.
+                ____currentOrder = MovementOrder.MovementOrderAdvance;
             }
         }
     }
