b3535f6cc4d937c80d532212aea2e6973439dfb8
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2024-03-27 00:49:39 +0800

    Finally fixed animation glitches.

--- BetterPikesMissionBehavior.cs
+++ BetterPikesMissionBehavior.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 
@@ -8,7 +7,6 @@ namespace BetterPikes
     public class BetterPikesMissionBehavior : MissionBehavior
     {
         private readonly ActionIndexCache _readyThrustActionIndex, _readyOverswingActionIndex, _guardUpActionIndex;
-        private readonly Dictionary<Agent, int> _glitchTicks;
 
         public override MissionBehaviorType BehaviorType => MissionBehaviorType.Other;
 
@@ -17,27 +15,21 @@ namespace BetterPikes
             _readyThrustActionIndex = ActionIndexCache.Create("act_ready_thrust_pike");
             _readyOverswingActionIndex = ActionIndexCache.Create("act_ready_overswing_pike");
             _guardUpActionIndex = ActionIndexCache.Create("act_guard_up_pike");
-            _glitchTicks = new Dictionary<Agent, int>();
         }
 
         public override void OnAgentBuild(Agent agent, Banner banner)
         {
             BetterPikesSettings settings = BetterPikesSettings.Instance;
 
-            if (agent.IsHuman)
+            // Determine which side to remove pikes from in siege battles.
+            if (Mission.IsSiegeBattle && agent.IsHuman && ((agent.Team.IsAttacker && settings.SidesToRemovePikes.SelectedIndex == 1) || (agent.Team.IsDefender && settings.SidesToRemovePikes.SelectedIndex == 2) || settings.SidesToRemovePikes.SelectedIndex == 3))
             {
-                _glitchTicks.Add(agent, 0);
-
-                // Determine which side to remove pikes from in siege battles.
-                if (Mission.IsSiegeBattle && ((agent.Team.IsAttacker && settings.SidesToRemovePikes.SelectedIndex == 1) || (agent.Team.IsDefender && settings.SidesToRemovePikes.SelectedIndex == 2) || settings.SidesToRemovePikes.SelectedIndex == 3))
+                for (EquipmentIndex index = EquipmentIndex.WeaponItemBeginSlot; index < EquipmentIndex.ExtraWeaponSlot; index++)
                 {
-                    for (EquipmentIndex index = EquipmentIndex.WeaponItemBeginSlot; index < EquipmentIndex.ExtraWeaponSlot; index++)
+                    if (IsPike(agent.Equipment[index]))
                     {
-                        if (IsPike(agent.Equipment[index]))
-                        {
-                            // Remove pikes in siege battles.
-                            agent.RemoveEquippedWeapon(index);
-                        }
+                        // Remove pikes in siege battles.
+                        agent.RemoveEquippedWeapon(index);
                     }
                 }
             }
@@ -46,24 +38,20 @@ namespace BetterPikes
         public override void OnAgentPanicked(Agent affectedAgent)
         {
             affectedAgent.SetScriptedFlags(affectedAgent.GetScriptedFlags() & ~Agent.AIScriptedFrameFlags.DoNotRun);
-            affectedAgent.SetActionChannel(1, ActionIndexCache.act_none, true);
+            affectedAgent.SetActionChannel(1, ActionIndexCache.act_none, ignorePriority: true, blendInPeriod: 0.5f);
         }
-
-        public override void OnAgentRemoved(Agent affectedAgent, Agent affectorAgent, AgentState agentState, KillingBlow blow) => _glitchTicks.Remove(affectedAgent);
 
         public override void OnMissionTick(float dt)
         {
             BetterPikesSettings settings = BetterPikesSettings.Instance;
 
-            foreach (Formation formation in Mission.Teams.SelectMany(team => team.FormationsIncludingSpecialAndEmpty.Where(f => f.QuerySystem.IsInfantryFormation && f.FiringOrder != FiringOrder.FiringOrderHoldYourFire)))
+            foreach (Formation formation in Mission.Teams.SelectMany(team => team.FormationsIncludingSpecialAndEmpty.Where(f => f.QuerySystem.IsInfantryFormation)))
             {
                 bool hasEnemy = formation.HasAnyEnemyFormationsThatIsNotEmpty() && formation.GetCountOfUnitsWithCondition(a => IsPike(a.WieldedWeapon)) >= formation.CountOfUnits * settings.MinPikemenPercentInPikeFormation && !formation.IsLoose;
                 bool isEnemyNearby = hasEnemy && formation.QuerySystem.AveragePosition.Distance(formation.QuerySystem.ClosestEnemyFormation.AveragePosition) <= settings.MinDistanceToReadyPikes;
 
-                foreach (Agent agent in formation.GetUnitsWithoutDetachedOnes())
+                foreach (Agent agent in formation.GetUnitsWithoutDetachedOnes().Where(a => !a.IsMainAgent))
                 {
-                    ActionIndexCache currentAction = agent.GetCurrentAction(1);
-
                     if (hasEnemy)
                     {
                         // If the pikemen have enemies, make the pikemen walk.
@@ -77,33 +65,29 @@ namespace BetterPikes
 
                     if (MBRandom.RandomFloat < 0.2f)
                     {
-                        if (isEnemyNearby && IsPike(agent.WieldedWeapon) && !agent.IsMainAgent)
-                        {
-                            agent.GetFormationFileAndRankInfo(out _, out int rankIndex);
+                        ActionIndexCache currentAction = agent.GetCurrentAction(1);
 
-                            // If the pikemen's enemies are nearby, make the pikemen ready their pikes in different positions.
-                            if (rankIndex < 4)
+                        if (isEnemyNearby && IsPike(agent.WieldedWeapon))
+                        {
+                            if (currentAction == ActionIndexCache.act_none)
                             {
-                                if (currentAction != _readyThrustActionIndex)
+                                agent.GetFormationFileAndRankInfo(out _, out int rankIndex);
+
+                                // If the pikemen's enemies are nearby, make the pikemen ready their pikes in different positions.
+                                if (rankIndex < 4)
                                 {
                                     // Make the first four ranks ready their pikes for an underarm thrust.
-                                    agent.SetActionChannel(1, _readyThrustActionIndex);
+                                    agent.SetActionChannel(1, _readyThrustActionIndex, startProgress: MBRandom.RandomFloat);
                                 }
-                            }
-                            else if (rankIndex == 4)
-                            {
-                                if (currentAction != _readyOverswingActionIndex)
+                                else if (rankIndex == 4)
                                 {
                                     // Make the fifth rank ready their pikes for an overhead thrust.
-                                    agent.SetActionChannel(1, _readyOverswingActionIndex);
+                                    agent.SetActionChannel(1, _readyOverswingActionIndex, startProgress: MBRandom.RandomFloat);
                                 }
-                            }
-                            else
-                            {
-                                if (currentAction != _guardUpActionIndex)
+                                else
                                 {
                                     // Make the sixth rank onwards ready their pikes at an angle.
-                                    agent.SetActionChannel(1, _guardUpActionIndex);
+                                    agent.SetActionChannel(1, _guardUpActionIndex, startProgress: MBRandom.RandomFloat);
                                 }
                             }
                         }
@@ -111,41 +95,12 @@ namespace BetterPikes
                         {
                             if (currentAction == _readyThrustActionIndex || currentAction == _readyOverswingActionIndex || currentAction == _guardUpActionIndex)
                             {
-                                agent.SetActionChannel(1, ActionIndexCache.act_none, true);
+                                agent.SetActionChannel(1, ActionIndexCache.act_none, ignorePriority: true, blendInPeriod: 0.5f);
                             }
                         }
                     }
-                }
-            }
 
-            foreach (Agent agent in Mission.Agents.Where(a => a.IsHuman))
-            {
-                agent.SetAgentFlags(IsPike(agent.WieldedWeapon) && !settings.CanPikemenBlock ? agent.GetAgentFlags() & ~AgentFlag.CanDefend : agent.GetAgentFlags() | AgentFlag.CanDefend);
-
-                if (_glitchTicks.TryGetValue(agent, out int numOfTicks) && IsPike(agent.WieldedWeapon))
-                {
-                    Agent.ActionCodeType currentActionType = agent.GetCurrentActionType(1);
-
-                    if (numOfTicks < 60 && agent.GetCurrentActionProgress(1) < 0.1f && (currentActionType == Agent.ActionCodeType.ReadyMelee || currentActionType == Agent.ActionCodeType.Guard))
-                    {
-                        _glitchTicks[agent]++;
-                    }
-                    else
-                    {
-                        if (numOfTicks >= 60)
-                        {
-                            agent.SetCurrentActionProgress(1, 1f);
-
-                            if (currentActionType != Agent.ActionCodeType.EquipUnequip)
-                            {
-                                agent.TryToSheathWeaponInHand(Agent.HandIndex.MainHand, Agent.WeaponWieldActionType.Instant);
-
-                                continue;
-                            }
-                        }
-
-                        _glitchTicks[agent] = 0;
-                    }
+                    agent.SetAgentFlags(agent.ImmediateEnemy != null && !agent.ImmediateEnemy.IsRunningAway && IsPike(agent.WieldedWeapon) && !settings.CanPikemenBlock ? agent.GetAgentFlags() & ~AgentFlag.CanDefend : agent.GetAgentFlags() | AgentFlag.CanDefend);
                 }
             }
         }
