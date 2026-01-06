f55a3f987a9fc6b3da61e336c2473fc38cac6f1d
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2026-01-06 11:10:02 +0800

    Enemies will now get knocked back if they enter an area between the pike head and the pikeman's left hand

--- BetterPikesMissionBehavior.cs
+++ BetterPikesMissionBehavior.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
@@ -41,62 +42,102 @@ namespace BetterPikes
 
         public override void OnMissionTick(float dt)
         {
-            foreach (Formation formation in Mission.Teams.SelectMany(team => team.FormationsIncludingSpecialAndEmpty.Where(f => f.CountOfUnits > 0 && f.QuerySystem.IsInfantryFormation)))
+            try
             {
-                bool isPikeFormation = formation.GetCountOfUnitsWithCondition(a => a.IsActive() && BetterPikesHelper.IsPike(a.WieldedWeapon)) >= formation.CountOfUnits * BetterPikesSettings.Instance.MinPikemenPercentInPikeFormation && formation.FiringOrder != FiringOrder.FiringOrderHoldYourFire;
-                bool hasEnemy = formation.HasAnyEnemyFormationsThatIsNotEmpty() && formation.CachedClosestEnemyFormation != null;
-                Vec2 positionOfClosestEnemyFormation = hasEnemy ? formation.CachedClosestEnemyFormation.Formation.CachedAveragePosition : Vec2.Invalid;
-                bool isEnemyNearby = hasEnemy && formation.CachedAveragePosition.Distance(positionOfClosestEnemyFormation) <= BetterPikesSettings.Instance.MaxDistanceToReadyPikes, isLoose = formation.IsLoose, isInCircleArrangement = formation.ArrangementOrder == ArrangementOrder.ArrangementOrderCircle;
-
-                foreach (Agent agent in formation.GetUnitsWithoutDetachedOnes().Where(a => a.IsHuman && a.IsActive()))
+                foreach (Formation formation in Mission.Teams.SelectMany(team => team.FormationsIncludingSpecialAndEmpty.Where(f => f.CountOfUnits > 0 && f.QuerySystem.IsInfantryFormation)))
                 {
-                    agent.SetScriptedFlags(agent.GetScriptedFlags() & ~Agent.AIScriptedFrameFlags.DoNotRun);
+                    bool isPikeFormation = formation.GetCountOfUnitsWithCondition(a => a.IsActive() && BetterPikesHelper.IsPike(a.WieldedWeapon)) >= formation.CountOfUnits * BetterPikesSettings.Instance.MinPikemenPercentInPikeFormation && formation.FiringOrder != FiringOrder.FiringOrderHoldYourFire;
+                    bool hasEnemy = formation.HasAnyEnemyFormationsThatIsNotEmpty() && formation.CachedClosestEnemyFormation != null;
+                    Vec2 positionOfClosestEnemyFormation = hasEnemy ? formation.CachedClosestEnemyFormation.Formation.CachedAveragePosition : Vec2.Invalid;
+                    bool isEnemyNearby = hasEnemy && formation.CachedAveragePosition.Distance(positionOfClosestEnemyFormation) <= BetterPikesSettings.Instance.MaxDistanceToReadyPikes, isLoose = formation.IsLoose;
+                    bool isInCircleArrangement = formation.ArrangementOrder == ArrangementOrder.ArrangementOrderCircle, isInSquareArrangement = formation.ArrangementOrder == ArrangementOrder.ArrangementOrderSquare;
 
-                    if (isPikeFormation)
+                    foreach (Agent agent in formation.GetUnitsWithoutDetachedOnes().Where(a => a.IsHuman && a.IsActive()))
                     {
-                        float distanceFromCurrentGlobalPosition = agent.Position.AsVec2.Distance(formation.GetCurrentGlobalPositionOfUnit(agent, true)), distanceFromClosestEnemyFormation = agent.Position.AsVec2.Distance(positionOfClosestEnemyFormation);
+                        agent.SetScriptedFlags(agent.GetScriptedFlags() & ~Agent.AIScriptedFrameFlags.DoNotRun);
 
-                        agent.SetMaximumSpeedLimit(agent.GetMaximumForwardUnlimitedSpeed(), false);
-
-                        if (hasEnemy && !isLoose && (distanceFromCurrentGlobalPosition < 2 || distanceFromClosestEnemyFormation <= 100))
+                        if (isPikeFormation)
                         {
-                            // If the pikemen have enemies, make the pikemen walk.
-                            agent.SetScriptedFlags(agent.GetScriptedFlags() | Agent.AIScriptedFrameFlags.DoNotRun);
-                        }
+                            float distanceFromCurrentGlobalPosition = agent.Position.AsVec2.Distance(formation.GetCurrentGlobalPositionOfUnit(agent, true)), distanceFromClosestEnemyFormation = agent.Position.AsVec2.Distance(positionOfClosestEnemyFormation);
 
-                        if (MBRandom.RandomFloat < 0.2f)
-                        {
-                            ActionIndexCache currentAction = agent.GetCurrentAction(1);
+                            agent.SetMaximumSpeedLimit(agent.GetMaximumForwardUnlimitedSpeed(), false);
 
-                            if ((isEnemyNearby || isInCircleArrangement) && BetterPikesHelper.IsPike(agent.WieldedWeapon) && agent.IsAIControlled && !agent.IsDoingPassiveAttack && distanceFromCurrentGlobalPosition < 3)
+                            if (hasEnemy && !isLoose && (distanceFromCurrentGlobalPosition < 2 || distanceFromClosestEnemyFormation <= 100))
                             {
-                                if (currentAction != _readyThrustActionIndex && currentAction != _guardUpActionIndex)
-                                {
-                                    agent.GetFormationFileAndRankInfo(out _, out int rankIndex);
+                                // If the pikemen have enemies, make the pikemen walk.
+                                agent.SetScriptedFlags(agent.GetScriptedFlags() | Agent.AIScriptedFrameFlags.DoNotRun);
+                            }
 
-                                    // If the pikemen's enemies are nearby, make the pikemen ready their pikes in different positions.
-                                    if (rankIndex < 5)
+                            if (MBRandom.RandomFloat < 0.2f)
+                            {
+                                ActionIndexCache currentAction = agent.GetCurrentAction(1);
+
+                                if (((isEnemyNearby && !isLoose) || isInCircleArrangement || isInSquareArrangement) && BetterPikesHelper.IsPike(agent.WieldedWeapon) && agent.IsAIControlled && !agent.IsDoingPassiveAttack && distanceFromCurrentGlobalPosition < 3)
+                                {
+                                    if (currentAction != _readyThrustActionIndex && currentAction != _guardUpActionIndex)
                                     {
-                                        // Make the first five ranks ready their pikes for a thrust.
-                                        agent.SetActionChannel(1, _readyThrustActionIndex, startProgress: MBRandom.RandomFloat);
-                                    }
-                                    else
-                                    {
-                                        // Make the sixth rank onwards ready their pikes at an angle.
-                                        agent.SetActionChannel(1, _guardUpActionIndex, startProgress: MBRandom.RandomFloat);
+                                        agent.GetFormationFileAndRankInfo(out _, out int rankIndex);
+
+                                        // If the pikemen's enemies are nearby, make the pikemen ready their pikes in different positions.
+                                        if (rankIndex < 5)
+                                        {
+                                            // Make the first five ranks ready their pikes for a thrust.
+                                            agent.SetActionChannel(1, _readyThrustActionIndex, startProgress: MBRandom.RandomFloat);
+                                        }
+                                        else
+                                        {
+                                            // Make the sixth rank onwards ready their pikes at an angle.
+                                            agent.SetActionChannel(1, _guardUpActionIndex, startProgress: MBRandom.RandomFloat);
+                                        }
                                     }
                                 }
-                            }
-                            else
-                            {
-                                if (currentAction == _readyThrustActionIndex || currentAction == _guardUpActionIndex)
+                                else
                                 {
-                                    agent.SetActionChannel(1, ActionIndexCache.act_none, ignorePriority: true, blendInPeriod: 0.5f);
+                                    if (currentAction == _readyThrustActionIndex || currentAction == _guardUpActionIndex)
+                                    {
+                                        agent.SetActionChannel(1, ActionIndexCache.act_none, ignorePriority: true, blendInPeriod: 0.5f);
+                                    }
                                 }
                             }
                         }
                     }
                 }
+
+                foreach (Agent agent in Mission.Agents.Where(a => a.IsHuman && a.IsActive() && BetterPikesHelper.IsPike(a.WieldedWeapon) && (a.GetCurrentAction(1).GetName().Contains("ready") || a.GetCurrentAction(1).GetName().Contains("release"))))
+                {
+                    float handleLength = agent.WieldedWeapon.Item.WeaponDesign.UsedPieces[2].ScaledLength, handleOffset = agent.WieldedWeapon.Item.WeaponDesign.UsedPieces[2].ScaledPieceOffset;
+                    // Find the frame of the agent's main hand.
+                    MatrixFrame mainHandFrame = agent.AgentVisuals.GetGlobalFrame().TransformToParent(agent.AgentVisuals.GetSkeleton().GetBoneEntitialFrameWithIndex(agent.Monster.MainHandItemBoneIndex));
+                    MatrixFrame handleFrontFrame = new MatrixFrame(mainHandFrame.rotation, mainHandFrame.origin).Elevate((handleLength * 0.4f) + handleOffset);
+                    MatrixFrame handleBackFrame = new MatrixFrame(mainHandFrame.rotation, mainHandFrame.origin).Elevate(handleLength * 0.2f);
+                    // Get the closest agent colliding with the pike handle.
+                    Agent collidedAgent = Mission.RayCastForClosestAgent(handleFrontFrame.origin, handleBackFrame.origin, -1, 0.04f, out _);
+
+                    if (collidedAgent != null && collidedAgent.IsHuman && collidedAgent.IsActive() && collidedAgent.IsEnemyOf(agent) && !collidedAgent.HasMount && !collidedAgent.IsInBeingStruckAction && !collidedAgent.GetCurrentAction(1).GetName().Contains("fall"))
+                    {
+                        Blow blow = new Blow(agent.Index);
+                        AttackCollisionData attackCollisionDataForDebugPurpose;
+
+                        blow.DamageType = DamageTypes.Blunt;
+                        blow.BoneIndex = collidedAgent.Monster.HeadLookDirectionBoneIndex;
+                        blow.GlobalPosition = collidedAgent.Position;
+                        blow.GlobalPosition.z = blow.GlobalPosition.z + collidedAgent.GetEyeGlobalHeight();
+                        blow.BaseMagnitude = BetterPikesSettings.Instance.PikeKnockbackMagnitude;
+                        blow.WeaponRecord.FillAsMeleeBlow(null, null, -1, -1);
+                        blow.InflictedDamage = 0;
+                        blow.SwingDirection = agent.LookDirection;
+                        blow.Direction = blow.SwingDirection;
+                        blow.DamageCalculated = false;
+                        blow.BlowFlag |= BlowFlags.KnockBack;
+                        attackCollisionDataForDebugPurpose = AttackCollisionData.GetAttackCollisionDataForDebugPurpose(false, false, false, true, false, false, false, false, false, false, false, false, CombatCollisionResult.StrikeAgent, -1, 0, 2, blow.BoneIndex, BoneBodyPartType.Chest, agent.Monster.MainHandItemBoneIndex, Agent.UsageDirection.AttackDown, -1, CombatHitResultFlags.NormalHit, 0.5f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, Vec3.Up, blow.Direction, blow.GlobalPosition, Vec3.Zero, Vec3.Zero, collidedAgent.Velocity, Vec3.Up);
+                        // Knock back the collided agent.
+                        collidedAgent.RegisterBlow(blow, attackCollisionDataForDebugPurpose);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                InformationManager.DisplayMessage(new InformationMessage(ex.ToString()));
             }
         }
     }
--- BetterPikesSubModule.cs
+++ BetterPikesSubModule.cs
@@ -1,6 +1,4 @@
 ﻿using HarmonyLib;
-using System;
-using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 
 namespace BetterPikes
@@ -8,28 +6,8 @@ namespace BetterPikes
     // This mod gives pikes realistic lengths, fixes the pike bracing animation so that the longer pikes can hit enemy cavalry, and makes pikemen use pike formations that are as functional as the game allows.
     public class BetterPikesSubModule : MBSubModuleBase
     {
-        private Harmony _harmony;
-        private Type _typeofPostureLogic;
-
-        protected override void OnSubModuleLoad()
-        {
-            _harmony = new Harmony("mod.bannerlord.betterpikes");
-            _harmony.PatchAll();
-        }
+        protected override void OnSubModuleLoad() => new Harmony("mod.bannerlord.betterpikes").PatchAll();
 
         public override void OnBeforeMissionBehaviorInitialize(Mission mission) => mission.AddMissionBehavior(new BetterPikesMissionBehavior());
-
-        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
-        {
-            _typeofPostureLogic = AccessTools.TypeByName("RBMAI.PostureLogic+CreateMeleeBlowPatch");
-            _harmony.Patch(AccessTools.Method(_typeofPostureLogic, "calculateDefenderPostureDamage"), postfix: new HarmonyMethod(AccessTools.Method(typeof(BetterPikesPostureLogic), "Postfix")));
-            _harmony.Patch(AccessTools.Method(_typeofPostureLogic, "calculateAttackerPostureDamage"), postfix: new HarmonyMethod(AccessTools.Method(typeof(BetterPikesPostureLogic), "Postfix")));
-        }
-
-        public override void OnGameEnd(Game game)
-        {
-            _harmony.Unpatch(AccessTools.Method(_typeofPostureLogic, "calculateDefenderPostureDamage"), AccessTools.Method(typeof(BetterPikesPostureLogic), "Postfix"));
-            _harmony.Unpatch(AccessTools.Method(_typeofPostureLogic, "calculateAttackerPostureDamage"), AccessTools.Method(typeof(BetterPikesPostureLogic), "Postfix"));
-        }
     }
 }
