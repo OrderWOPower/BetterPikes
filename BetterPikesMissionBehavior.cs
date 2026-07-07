ab5635d1f61eb1df8347f05101fb4ac75006b648
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2026-07-08 04:02:08 +0800

    Fixed a conflict with AI Kick N Bash

--- BetterPikesMissionBehavior.cs
+++ BetterPikesMissionBehavior.cs
@@ -22,7 +22,6 @@ namespace BetterPikes
 			if (agent.IsHuman)
 			{
 				agent.AddComponent(new BetterPikesAgentComponent(agent));
-				agent.SetHasOnAiInputSetCallback(true);
 			}
 		}
 
@@ -38,35 +37,40 @@ namespace BetterPikes
 		{
 			foreach (Agent agent in Mission.Agents.FindAll(a => a.IsHuman && a.IsActive() && !a.HasMount))
 			{
-				if (BetterPikesHelper.IsWieldingPike(agent) && (agent.GetCurrentActionType(1) == Agent.ActionCodeType.ReadyMelee || agent.GetCurrentActionType(1) == Agent.ActionCodeType.ReleaseMelee) && BetterPikesSettings.Instance.DoPikesHaveCollision)
+				if (BetterPikesHelper.IsWieldingPike(agent))
 				{
-					float handleLength = BetterPikesHelper.GetWieldedWeapon(agent).Item.WeaponDesign.UsedPieces[2].ScaledLength, handleOffset = BetterPikesHelper.GetWieldedWeapon(agent).Item.WeaponDesign.UsedPieces[2].ScaledPieceOffset;
-					// Find the frame of the agent's main hand.
-					MatrixFrame mainHandFrame = agent.AgentVisuals.GetGlobalFrame().TransformToParent(agent.AgentVisuals.GetSkeleton().GetBoneEntitialFrameWithIndex(agent.Monster.MainHandItemBoneIndex));
-					MatrixFrame handleFrontFrame = new MatrixFrame(mainHandFrame.rotation, mainHandFrame.origin).Elevate((handleLength * 0.4f) + handleOffset);
-					MatrixFrame handleBackFrame = new MatrixFrame(mainHandFrame.rotation, mainHandFrame.origin).Elevate((handleLength * 0.3f) + handleOffset);
-					// Get the closest agent colliding with the pike handle.
-					Agent collidedAgent = Mission.RayCastForClosestAgent(handleFrontFrame.origin, handleBackFrame.origin, -1, 0.04f, out _);
+					agent.SetHasOnAiInputSetCallback(true);
 
-					if (collidedAgent != null && collidedAgent.IsHuman && collidedAgent.IsActive() && collidedAgent.IsEnemyOf(agent) && !collidedAgent.HasMount && !collidedAgent.IsInBeingStruckAction && collidedAgent.GetCurrentActionType(1) != Agent.ActionCodeType.Fall)
+					if ((agent.GetCurrentActionType(1) == Agent.ActionCodeType.ReadyMelee || agent.GetCurrentActionType(1) == Agent.ActionCodeType.ReleaseMelee) && BetterPikesSettings.Instance.DoPikesHaveCollision)
 					{
-						Blow blow = new Blow(agent.Index);
-						AttackCollisionData attackCollisionDataForDebugPurpose;
+						float handleLength = BetterPikesHelper.GetWieldedWeapon(agent).Item.WeaponDesign.UsedPieces[2].ScaledLength, handleOffset = BetterPikesHelper.GetWieldedWeapon(agent).Item.WeaponDesign.UsedPieces[2].ScaledPieceOffset;
+						// Find the frame of the agent's main hand.
+						MatrixFrame mainHandFrame = agent.AgentVisuals.GetGlobalFrame().TransformToParent(agent.AgentVisuals.GetSkeleton().GetBoneEntitialFrameWithIndex(agent.Monster.MainHandItemBoneIndex));
+						MatrixFrame handleFrontFrame = new MatrixFrame(mainHandFrame.rotation, mainHandFrame.origin).Elevate((handleLength * 0.4f) + handleOffset);
+						MatrixFrame handleBackFrame = new MatrixFrame(mainHandFrame.rotation, mainHandFrame.origin).Elevate((handleLength * 0.3f) + handleOffset);
+						// Get the closest agent colliding with the pike handle.
+						Agent collidedAgent = Mission.RayCastForClosestAgent(handleFrontFrame.origin, handleBackFrame.origin, -1, 0.04f, out _);
 
-						blow.DamageType = DamageTypes.Blunt;
-						blow.BoneIndex = collidedAgent.Monster.HeadLookDirectionBoneIndex;
-						blow.GlobalPosition = collidedAgent.Position;
-						blow.GlobalPosition.z += collidedAgent.GetEyeGlobalHeight();
-						blow.BaseMagnitude = 0;
-						blow.WeaponRecord.FillAsMeleeBlow(null, null, -1, -1);
-						blow.InflictedDamage = 0;
-						blow.SwingDirection = agent.LookDirection;
-						blow.Direction = blow.SwingDirection;
-						blow.DamageCalculated = true;
-						blow.BlowFlag |= BlowFlags.KnockBack;
-						attackCollisionDataForDebugPurpose = AttackCollisionData.GetAttackCollisionDataForDebugPurpose(false, false, false, true, false, false, false, false, false, false, false, false, CombatCollisionResult.StrikeAgent, -1, 0, 2, blow.BoneIndex, BoneBodyPartType.Chest, agent.Monster.MainHandItemBoneIndex, Agent.UsageDirection.AttackDown, -1, CombatHitResultFlags.NormalHit, 0.5f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, Vec3.Up, blow.Direction, blow.GlobalPosition, Vec3.Zero, Vec3.Zero, collidedAgent.Velocity, Vec3.Up);
-						// Knock back the collided agent.
-						collidedAgent.RegisterBlow(blow, attackCollisionDataForDebugPurpose);
+						if (collidedAgent != null && collidedAgent.IsHuman && collidedAgent.IsActive() && collidedAgent.IsEnemyOf(agent) && !collidedAgent.HasMount && !collidedAgent.IsInBeingStruckAction && collidedAgent.GetCurrentActionType(1) != Agent.ActionCodeType.Fall)
+						{
+							Blow blow = new Blow(agent.Index);
+							AttackCollisionData attackCollisionDataForDebugPurpose;
+
+							blow.DamageType = DamageTypes.Blunt;
+							blow.BoneIndex = collidedAgent.Monster.HeadLookDirectionBoneIndex;
+							blow.GlobalPosition = collidedAgent.Position;
+							blow.GlobalPosition.z += collidedAgent.GetEyeGlobalHeight();
+							blow.BaseMagnitude = 0;
+							blow.WeaponRecord.FillAsMeleeBlow(null, null, -1, -1);
+							blow.InflictedDamage = 0;
+							blow.SwingDirection = agent.LookDirection;
+							blow.Direction = blow.SwingDirection;
+							blow.DamageCalculated = true;
+							blow.BlowFlag |= BlowFlags.KnockBack;
+							attackCollisionDataForDebugPurpose = AttackCollisionData.GetAttackCollisionDataForDebugPurpose(false, false, false, true, false, false, false, false, false, false, false, false, CombatCollisionResult.StrikeAgent, -1, 0, 2, blow.BoneIndex, BoneBodyPartType.Chest, agent.Monster.MainHandItemBoneIndex, Agent.UsageDirection.AttackDown, -1, CombatHitResultFlags.NormalHit, 0.5f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, Vec3.Up, blow.Direction, blow.GlobalPosition, Vec3.Zero, Vec3.Zero, collidedAgent.Velocity, Vec3.Up);
+							// Knock back the collided agent.
+							collidedAgent.RegisterBlow(blow, attackCollisionDataForDebugPurpose);
+						}
 					}
 				}
 
@@ -79,10 +83,9 @@ namespace BetterPikes
 			foreach (Formation formation in Mission.Teams.SelectMany(team => team.FormationsIncludingSpecialAndEmpty.Where(f => BetterPikesHelper.IsPikeFormation(f))))
 			{
 				float closestEnemyFormationDistanceSquared = formation.CachedClosestEnemyFormationDistanceSquared, formationWidth = formation.Width;
-				bool hasEnemy = formation.HasAnyEnemyFormationsThatIsNotEmpty() && formation.CachedClosestEnemyFormation != null, isLoose = formation.IsLoose;
-				bool isEnemyNearby = closestEnemyFormationDistanceSquared <= MathF.Pow(BetterPikesSettings.Instance.MaxDistanceToReadyPikes, 2);
+				bool hasEnemy = formation.HasAnyEnemyFormationsThatIsNotEmpty(), isLoose = formation.IsLoose, isEnemyNearby = closestEnemyFormationDistanceSquared <= MathF.Pow(BetterPikesSettings.Instance.MaxDistanceToReadyPikes, 2);
 				bool isInCircleArrangement = formation.ArrangementOrder == ArrangementOrder.ArrangementOrderCircle, isInSquareArrangement = formation.ArrangementOrder == ArrangementOrder.ArrangementOrderSquare;
-				Vec2 formationPosition = formation.CachedAveragePosition, closestEnemyFormationPosition = hasEnemy ? formation.CachedClosestEnemyFormation.Formation.CachedAveragePosition : Vec2.Invalid;
+				Vec2 formationPosition = formation.CachedAveragePosition, closestEnemyFormationPosition = formation.CachedClosestEnemyFormation != null ? formation.CachedClosestEnemyFormation.Formation.CachedAveragePosition : Vec2.Invalid;
 
 				foreach (Agent agent in formation.GetUnitsWithoutDetachedOnes().Where(a => a.IsHuman && a.IsActive()))
 				{
